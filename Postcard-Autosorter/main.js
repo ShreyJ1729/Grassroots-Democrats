3e15556d7a9d0dda5e129c789a965c421f026372
Shrey Joshi <shreyjoshi2004@gmail.com>
2020-11-12 00:40:56 -0600

    added functionality

--- Postcard-Autosorter/main.js
+++ Postcard-Autosorter/main.js
@@ -1,29 +1,67 @@
-const { app, BrowserWindow } = require('electron')
+const {app, BrowserWindow, dialog, ipcMain} = require('electron')
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 1980,
-    height: 1080,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-  win.maximize()
-  win.loadFile('index.html')
-//   win.webContents.openDevTools()
+const url = require('url')
+const path = require('path')
+const fs=require('fs')
+
+
+// const {dialog}=require('electron')
+
+let win
+
+function createWindow() {
+   win = new BrowserWindow({
+     width: 800,
+     height: 600,
+     webPreferences: { 
+      nodeIntegration: true,
+     }
+    })
+   win.loadURL(url.format ({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+   }))
+   win.openDevTools();
+  //  win.maximize();
 }
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+ipcMain.handle('signup-click-button', (event,arg)=>{
+    if(arg=='true'){
+      console.log("got button click");
+
+      dialog.showOpenDialog()
+      .then( (result) => {
+        const filenames = result.filePaths;
+
+        if(filenames==undefined) {
+          console.log("No file selected");
+        }
+        else {
+          readFile(filenames[0])
+          win.loadURL(`file://${__dirname}/up_address.html`);
+        }
+      }).catch(err => {
+        console.log(err)
+      })
+
+    }
 })
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
+let signup_data;
 
+function readFile(filepath){
+  console.log("reading file...");
+  console.log(filepath);
+  fs.readFile(filepath,'utf-8',(err,data)=>{
+    signup_data = data;
+    if(err){
+      alert("An error :"+err.message)
+      return
+    }
+    win.webContents.send('fileData',data);
+  })
 
-app.whenReady().then(createWindow)
+}
+
+app.on('ready', createWindow);
